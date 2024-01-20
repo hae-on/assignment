096e9efc91ce97ac34e35b9acd8baf5d0522f98b
hae-on <solbi2004@naver.com>
2024-01-20 21:46:55 +0900

    feat: 3초 후 order 페이지로 이동 구현

--- src/pages/CompletePage.tsx
+++ src/pages/CompletePage.tsx
@@ -1,5 +1,40 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
+import { CheckIcon } from 'src/assets/Icons';
+
 export const CompletePage = () => {
-  return <div>completePage</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/order');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+
+  return (
+    <CompletePageContainer>
+      <CheckIcon />
+      <p>주문이 완료되었습니다.</p>
+    </CompletePageContainer>
+  );
 };
 
 export default CompletePage;
+
+const CompletePageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 85%;
+
+  > p {
+    margin-top: 10px;
+  }
+`;
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,38 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/order');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [navigate]);
+
+  return (
+    <ErrorPageContainer>
+      <p>주문에 실패하였습니다.</p>
+      <p>다시 시도해주세요.</p>
+    </ErrorPageContainer>
+  );
 };
 
 export default ErrorPage;
+
+const ErrorPageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 85%;
+
+  > p {
+    margin-bottom: 10px;
+  }
+`;
