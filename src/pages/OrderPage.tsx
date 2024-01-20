7da8af91d8d4417ff0ceb083562b44fd3bb4dd32
hae-on <solbi2004@naver.com>
2024-01-20 18:07:45 +0900

    feat: 로딩 상태 구현

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -8,12 +8,16 @@ import ProductList from 'src/components/Order/ProductList';
 import type { Product } from 'src/types/product';
 
 export const OrderPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     const fetchProduct = async () => {
       const data = await fetchOrderProducts();
       setProducts(data);
+      setIsLoading(false);
     };
 
     fetchProduct();
@@ -23,7 +27,16 @@ export const OrderPage = () => {
     <>
       <Header />
       <OrderPageContainer>
-        <ProductList products={products} />
+        {isLoading ? (
+          <LoadingMessageWrapper>
+            <LoadingMessage>
+              목록을 <br />
+              불러오고 있습니다.
+            </LoadingMessage>
+          </LoadingMessageWrapper>
+        ) : (
+          <ProductList products={products} />
+        )}
       </OrderPageContainer>
       <OrderInfoBox />
     </>
@@ -35,4 +48,15 @@ export default OrderPage;
 const OrderPageContainer = styled.section`
   margin: 30px 0 270px 0;
   padding: 25px;
+`;
+
+const LoadingMessageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+`;
+
+const LoadingMessage = styled.p`
+  text-align: center;
 `;
